4b022de3727aeea20bb7070b6674f65b33c74f57
Chulju, Hong <ang0123dev@gmail.com>
2013-06-09 16:49:43 +0900

    info_ui added

---
+++ akari_main/info_ui.h
@@ -0,0 +1,26 @@
+﻿// Ŭnicode please
+#ifndef __INFO_UI_H__
+#define __INFO_UI_H__
+
+class Camera;
+class FTGLPixmapFont;
+typedef std::unique_ptr<FTGLPixmapFont> FontPtr;
+
+namespace akari {;
+
+class InfoUI {
+public:
+    InfoUI();
+    ~InfoUI();
+
+public:
+    void DrawCameraInfo(const Camera &camera);
+    void DrawAxis();
+
+private:
+    FontPtr font_;
+};
+
+};
+
+#endif
